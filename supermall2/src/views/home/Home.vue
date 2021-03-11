0fd96ddc2130328ae87a0fc9d1b43085a7bc1cd0
JLdiesel <1317662314@qq.com>
2021-03-11 10:50:30 +0800

    123

--- supermall2/src/views/home/Home.vue
+++ supermall2/src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2 style="color:white">首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <!-- <recommend-view :recommends="recommends" /> -->
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childrenComps/HomeSwiper";
+import RecommendView from "./childrenComps/RecommendView.vue";
+
+import { getHomeMultidata } from "network/home.js";
 export default {
-    name:'Home'
-}
+  components: { NavBar, HomeSwiper, RecommendView },
+  name: "Home",
+
+  RecommondViewdata() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    getHomeMultidata()
+      .then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.home-nav {
+  color: white;
+  background-color: var(--color-tint);
+}
 </style>
